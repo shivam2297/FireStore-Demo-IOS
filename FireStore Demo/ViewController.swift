aafa58ecd857c059df412de3cef2f834c4847b74
developer1 <sshrivastav@contentactive.com>
2019-03-08 10:12:24 +0530

    Initial Commit

---
+++ FireStore Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireStore Demo
+//
+//  Created by Daffolspmac-67 on 08/03/19.
+//  Copyright © 2019 Daffolspmac-67. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
